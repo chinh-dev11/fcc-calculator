9016fde0191225d7087848977b0db2decc5a687e
Chinh Le <chinh.le11@gmail.com>
2020-03-31 13:52:03 -0400

    Define elements

---
+++ src/containers/CalculatorComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import './CalculatorComponent.scss';
+
+const CalculatorComponent = () => (
+  <Container fluid>
+    <h1>Calculator</h1>
+  </Container>
+);
+
+export default CalculatorComponent;
