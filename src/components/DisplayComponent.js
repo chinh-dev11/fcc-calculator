3914ae9fc1c8d6642bb7fa4f2fd84eb45728d818
Chinh Le <chinh.le11@gmail.com>
2020-04-02 15:44:52 -0400

    Add functionality

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const DisplayComponent = (props) => {
   const { expression, total } = { ...props };
+  const reNumOp = /((-?)([\d]*)([+\-*/]?))/g;
+  const expressionDisplay = expression.match(reNumOp).reduce((acc, curr) => {
+    if (curr) {
+      const reOp = /[+\-*/]$/g;
+      if (reOp.test(curr)) {
+        return acc.concat(curr.substr(0, curr.length - 1), ' ', curr.substr(curr.length - 1), ' ');
+      }
+      return acc.concat(curr);
+    }
+    return acc;
+  }, []);
+
   return (
-    <div>
-      <div>{expression}</div>
-      <div>{total}</div>
+    <div className="text-right" style={{ width: '100%' }}>
+      <div style={{ height: '16px' }}>{expressionDisplay}</div>
+      <h2>{total}</h2>
     </div>
   );
 };
--- src/containers/CalculatorComponent.js
+++ src/containers/CalculatorComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import {
+  Container, Col, Row,
+} from 'react-bootstrap';
 import { evaluate } from 'mathjs';
 
-import PadComponent from '../components/PadComponent';
-import KeyComponent from '../components/KeyComponent';
+import ButtonComponent from '../components/ButtonComponent';
 import DisplayComponent from '../components/DisplayComponent';
 import './CalculatorComponent.scss';
 
@@ -11,6 +12,8 @@ class CalculatorComponent extends React.Component {
   constructor(props) {
     super(props);
     this.initialState = {
+      lastKeyStroke: '',
+      display: '',
       expression: '',
       total: 0,
     };
@@ -22,18 +25,27 @@ class CalculatorComponent extends React.Component {
   }
 
   totalHandler() {
-    // console.log(evaluate(this.state.expression));
-    this.setState((prevState) => ({
-      total: evaluate(prevState.expression),
-    }));
+    const reExpression = /([\d.+\-*/]*)([\d.])/g;
+
+    this.setState((prevState) => {
+      const expressionValidated = prevState.expression.match(reExpression);
+      return {
+        total: evaluate(expressionValidated[0]),
+        expression: expressionValidated[0],
+        lastKeyStroke: 'total',
+      };
+    });
   }
 
   operatorHandler(evt) {
     // console.log(evt.target.value);
     evt.persist();
+    const reOp = /([+\-*/]$)|([+\-*/][-?]$)/;
+    const opValidated = evt.target.value.match(reOp);
 
     this.setState((prevState) => ({
-      expression: prevState.expression.concat(evt.target.value),
+      expression: prevState.expression.concat(opValidated[0]),
+      lastKeyStroke: '',
     }));
   }
 
@@ -43,8 +55,12 @@ class CalculatorComponent extends React.Component {
     // https://reactjs.org/docs/events.html#event-pooling
     evt.persist(); // the warning is caused by the setState async call below
 
+    const reNum = /([^0*])(\d*)(.?)(\d*)/;
+    const numValidated = evt.target.value.match(reNum);
+
     this.setState((prevState) => ({
-      expression: prevState.expression.concat(evt.target.value),
+      expression: prevState.lastKeyStroke === 'total' ? numValidated[0] : prevState.expression.concat(numValidated[0]),
+      lastKeyStroke: '',
     }));
   }
 
@@ -54,17 +70,125 @@ class CalculatorComponent extends React.Component {
 
   render() {
     const { expression, total } = { ...this.state };
+    const btns = {
+      row1: [
+        {
+          type: 'AC',
+          label: 'AC',
+          handler: this.clearHandler,
+        },
+        {
+          type: '/',
+          label: '/',
+          handler: this.operatorHandler,
+        },
+        {
+          type: '*',
+          label: 'x',
+          handler: this.operatorHandler,
+        },
+      ],
+      row2: [
+        {
+          type: '7',
+          label: '7',
+          handler: this.numberHandler,
+        },
+        {
+          type: '8',
+          label: '8',
+          handler: this.numberHandler,
+        },
+        {
+          type: '9',
+          label: '9',
+          handler: this.numberHandler,
+        },
+        {
+          type: '-',
+          label: '-',
+          handler: this.operatorHandler,
+        },
+      ],
+      row3: [
+        {
+          type: '4',
+          label: '4',
+          handler: this.numberHandler,
+        },
+        {
+          type: '5',
+          label: '5',
+          handler: this.numberHandler,
+        },
+        {
+          type: '6',
+          label: '6',
+          handler: this.numberHandler,
+        },
+        {
+          type: '+',
+          label: '+',
+          handler: this.operatorHandler,
+        },
+      ],
+      row4: [
+        {
+          type: '1',
+          label: '1',
+          handler: this.numberHandler,
+        },
+        {
+          type: '2',
+          label: '2',
+          handler: this.numberHandler,
+        },
+        {
+          type: '3',
+          label: '3',
+          handler: this.numberHandler,
+        },
+      ],
+      row5: [
+        {
+          type: '0',
+          label: '0',
+          handler: this.numberHandler,
+        },
+        {
+          type: '.',
+          label: '.',
+          handler: this.numberHandler,
+        },
+      ],
+      col4: [
+        {
+          type: '=',
+          label: '=',
+          handler: this.totalHandler,
+        },
+      ],
+    };
+    const btnsRow1 = btns.row1.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+    const btnsRow2 = btns.row2.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+    const btnsRow3 = btns.row3.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+    const btnsRow4 = btns.row4.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+    const btnsCol4 = btns.col4.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+    const btnsRow5 = btns.row5.map((btn) => <ButtonComponent key={btn.type} type={btn.type} label={btn.label} handler={btn.handler} />);
+
     return (
       <Container fluid>
-        <h1>Calculator</h1>
-        <DisplayComponent expression={expression} total={total} />
-        <PadComponent handler={this.numberHandler} />
-        <KeyComponent value="+" handler={this.operatorHandler} />
-        <KeyComponent value="-" handler={this.operatorHandler} />
-        <KeyComponent value="*" handler={this.operatorHandler} />
-        <KeyComponent value="/" handler={this.operatorHandler} />
-        <KeyComponent value="=" handler={this.totalHandler} />
-        <KeyComponent value="AC" handler={this.clearHandler} />
+        <Row className="calc-container mx-auto" style={{ width: '240px' }}>
+          <DisplayComponent expression={expression} total={total} />
+          <div>{btnsRow1}</div>
+          <div>{btnsRow2}</div>
+          <div>{btnsRow3}</div>
+          <Col className="p-0">
+            {btnsRow4}
+            {btnsRow5}
+          </Col>
+          <div>{btnsCol4}</div>
+        </Row>
       </Container>
     );
   }
